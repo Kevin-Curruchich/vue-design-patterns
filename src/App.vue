f9eac2f52ffe4ca24caecb30e0a1b6e3caff0387
kevincxdev <kevincxdev@gmail.com>
2025-07-02 04:42:35 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import StrategyPattern from './views/StrategyPattern.vue';
+
+</script>
+
+<template>
+  <StrategyPattern />
+</template>
+
+<style scoped>
+</style>
---
+++ src/views/StrategyPattern.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h2>
+        Strategy Pattern
+    </h2>
+    <p> 
+        <b>
+            Overview: 
+        </b>
+        Is a design pattern that let define a family of algorithms, put each of them into a separate class, and make their objects interchangeable
+        <quote>
+            
+        </quote>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+</style>
